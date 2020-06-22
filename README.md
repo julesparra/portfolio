9804946c34f3c9b40c7d2268090c14c666ed7674
Jules Parra <jparra@terpmail.umd.edu>
2020-06-22 17:25:17 -0400

    Deleted bootstrap and javascript

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # julesparra
-✨Jules Parra's professional website!
+👋🏼Jules Parra's professional website!
+
+✨Fonts
+Headings: Anonymous Pro, monospace
+Copy: Roboto, sans serif
+
+⚡️Colors
+Grey background: #CACACA
+Green accent: #95D180
+Blue accent: #80BED1
+Clay accent: #D19480
+LinkedIn blue: #007BB5
+Facebook blue: #3B5998
+Instagram purple: #D327C3
--- js/about.js
+++ js/about.js
@@ -1,15 +1,26 @@
-$(function(){
-    $("#prompt").dialog({
-        modal: true,
-        buttons:{
-            Submit: function(){
-                $(this).dialog("close");
-            }
-        },
-        hide:{
-            effect: "clip",
-            duration: 1000
-        }
-            
-    });
-});
+//TEMPORARY -- MODAL HTML & CSS NOT FINALIZED
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
